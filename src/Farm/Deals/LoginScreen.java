83a3088ec86fdb3dd21e4a3a7aa61ab63aa07ca8
Aristeidis Nikolopoulos <4rispot@gmail.com>
2014-12-06 22:40:46 +0200

    Farmdeals version 1.2 with Activity code and updates on Dao.java, Main.java and the layout .xmls.

---
+++ src/Farm/Deals/LoginScreen.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Farm.Deals;
+
+//import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ * @author aris
+ */
+public class LoginScreen extends Main { 
+       @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        this.setContentView(R.layout.login);
+    }
+}
