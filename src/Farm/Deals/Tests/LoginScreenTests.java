2388b031d03a544cad1044df9f3753eb0e6bc006
kiki92 <kikiiakwvou@gmail.com>
2015-01-18 22:32:24 +0200

    Create LoginScreenTests.java

---
+++ src/Farm/Deals/Tests/LoginScreenTests.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Farm.Deals.Tests;
+
+
+import Farm.Deals.LoginScreen;
+import Farm.Deals.R;
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.widget.Button;
+
+public class LoginScreenTests extends ActivityUnitTestCase<LoginScreen> {
+Intent mLaunchIntent;
+    public LoginScreenTests(Class<LoginScreen> activityClass) {
+        super(activityClass);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mLaunchIntent = new Intent(getInstrumentation()
+                .getTargetContext(), LoginScreen.class);
+        startActivity(mLaunchIntent, null, null);
+        final Button launchNextButton =
+                (Button) getActivity()
+                .findViewById(R.id.farmerProductsScreen);
+    }
+    
+    @MediumTest
+public void testNextActivityWasLaunchedWithIntent() {
+    startActivity(mLaunchIntent, null, null);
+    final Button launchNextButton =
+            (Button) getActivity()
+            .findViewById(R.id.farmerProductsScreen);
+    launchNextButton.performClick();
+
+    final Intent launchIntent = getStartedActivityIntent();
+    assertNotNull("Intent was null", launchIntent);
+    assertTrue(isFinishCalled());
+
+    }
+}
+
